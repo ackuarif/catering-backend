61967344c9939dcc54c4ac7a26f05a5df0051521
Achmad Kumail Arif <ackuarif@gmail.com>
2023-06-05 07:34:49 +0700

    update limit upload

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(
 
 app.set('trust proxy', true);
 
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '50mb', extended: true }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 app.use("/api/admin", AdminRoutes);
 app.use("/api/pelanggan", PelangganRoutes);
